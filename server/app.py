34145a4eeac813ccc1a35c82eb3a98faa427b33a
Sam .M <you@example.com>
2023-09-22 20:32:21 +0300

    done

--- server/app.py
+++ server/app.py
@@ -2,15 +2,39 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Create a SQLAlchemy instance and pass it the app
+db = SQLAlchemy(app)
+
+# Initialize the migration engine
 migrate = Migrate(app, db)
-
-db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555)
+
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
